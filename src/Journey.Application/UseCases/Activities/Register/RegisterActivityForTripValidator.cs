11554c8cd7b38104e0454dfd42e351a1d216dfcd
devbygabriel <devbygabriel@outlook.com>
2024-07-11 16:50:30 -0300

    Adicionar arquivos de projeto.

---
+++ src/Journey.Application/UseCases/Activities/Register/RegisterActivityForTripValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Journey.Communication.Requests;
+using Journey.Exception;
+
+namespace Journey.Application.UseCases.Activities.Register
+{
+    public class RegisterActivityForTripValidator : AbstractValidator<RequestRegisterActivityJson>
+    {
+        public RegisterActivityForTripValidator() 
+        {
+            RuleFor(request => request.Name)
+                .NotEmpty()
+                .WithMessage(ResourceErrorMessages.NAME_EMPTY);
+
+        }
+    }
+}
---
+++ src/Journey.Exception/ExceptionsBase/JourneyException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Journey.Exception.ExceptionsBase
+{
+    public abstract class JourneyException : SystemException
+    {
+        public JourneyException(String message) : base(message)
+        {
+            
+        }
+
+        public abstract HttpStatusCode GetStatusCode();
+        public abstract IList<string> GetErrorMessages();
+
+    }
+}
